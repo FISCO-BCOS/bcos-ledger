1956c515fbcd68cdd6721173d77c9d19f41ed846
MO NAN <651932351@qq.com>
2021-05-31 10:24:28 +0800

    use URL_BASE insted of FETCH_URL_BASE (#14)

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,5 +1,5 @@
 hunter_config(bcos-framework VERSION 3.0.0-61f2159aaac9994591e3b66c1882be97960b160e
-        URL https://github.com/FISCO-BCOS/bcos-framework/archive/61f2159aaac9994591e3b66c1882be97960b160e.tar.gz
+        URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/61f2159aaac9994591e3b66c1882be97960b160e.tar.gz
         SHA1 3f6ad1af8b46d93bc0c4f067e5aa2562a65e26af
         CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
