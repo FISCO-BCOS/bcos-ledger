070fa112d747a2379447ea568484119f0bfc3814
bxq2011hust <bxq2011hust@qq.com>
2021-11-30 14:38:21 +0800

    update framework and cmake scripts

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,14 +1,14 @@
 hunter_config(bcos-framework VERSION 3.0.1-local
-        URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/58f6baa2e45fa18f617da189d3bd5420184f4e32.tar.gz
-        SHA1 71b2db1b0e02d36bc7b3f8a8cdb0a4c79d7e1a86
+        URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/2a3beb81f02fc4c5cdd39c333ae5021b12a6d708.tar.gz
+	SHA1 9bd7b4be1511ce6321f8d4b26c1dff8af11e3729
         CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON HUNTER_KEEP_PACKAGE_SOURCES=ON #DEBUG=ON
 )
 
-hunter_config(wedpr-crypto VERSION 1.1.0-10f314de
-        URL https://${URL_BASE}/WeBankBlockchain/WeDPR-Lab-Crypto/archive/10f314de45ec31ce9e330922b522ce173662ed33.tar.gz
-        SHA1 626df59f87ea2c6bb5128f7d104588179809910b
-        CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=OFF HUNTER_PACKAGE_LOG_INSTALL=ON HUNTER_KEEP_PACKAGE_SOURCES=ON
-)
+# hunter_config(wedpr-crypto VERSION 1.1.0-10f314de
+#         URL https://${URL_BASE}/WeBankBlockchain/WeDPR-Lab-Crypto/archive/10f314de45ec31ce9e330922b522ce173662ed33.tar.gz
+#         SHA1 626df59f87ea2c6bb5128f7d104588179809910b
+#         CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=OFF HUNTER_PACKAGE_LOG_INSTALL=ON HUNTER_KEEP_PACKAGE_SOURCES=ON
+# )
 
 hunter_config(bcos-crypto
         VERSION 3.0.0-local-43df7523
